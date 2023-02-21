caf5093b1f1ba85224097f93ff5db5f14ea60622
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-21 10:17:14 +0500

    Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿//Task43
+Console.Clear();
+Console.Write("Введите значения k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значения b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Результат: ({x}, {y})");
