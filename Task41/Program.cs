3eca22e735d7545ce8eb282a3d43c99b93e225ff
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-21 10:16:36 +0500

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿//Task 41
+Console.Clear();
+Console.Write("Укажите кол-во вводимых чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++; // count = count + 1;
+}
+Console.WriteLine($"Результат: {count}");
